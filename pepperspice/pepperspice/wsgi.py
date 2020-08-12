b951a7748e7f3b22a70f97b8e1f69298d8c78c35
pc <mikej2279@gmail.com>
2020-08-12 02:17:29 +0200

    commit

--- pepperspice/pepperspice/wsgi.py
+++ pepperspice/pepperspice/wsgi.py
@@ -8,7 +8,25 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import time 
+import traceback 
+import signal 
+import sys 
+
 from django.core.wsgi import get_wsgi_application
+
+ 
+sys.path.append('/var/www/pepperspice-web/pepperspice') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/usr/bin/python3.6') 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pepperspice.settings')
 
-application = get_wsgi_application()
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
