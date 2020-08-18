056a77d8457bb712b3fbc7f2735bb0f55b8b4f0b
edvin <mikej2279@gmail.com>
2020-08-18 21:05:42 +0200

    commit

--- pepperspice/dashboard/views.py
+++ pepperspice/dashboard/views.py
@@ -295,6 +295,7 @@ def new_db(request):
                         db.save()
 
 
+
                         a = DBNode.objects.filter(db_uid=DB_Details.db_uid).first()
                         a.db_internal_ip = os.popen("echo esxiforme | sudo -S docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' " + DB_Details.db_uid).read().strip()
                         a.save()
