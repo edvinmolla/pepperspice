550d5c6db83a6a63e8f55cc6f78a0a5589c0d37b
pc <mikej2279@gmail.com>
2020-10-04 03:31:56 +0200

    password reset done

--- pepperspice/pepperspice/settings.py
+++ pepperspice/pepperspice/settings.py
@@ -70,9 +70,23 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'dashboard.apps.DashboardConfig',
     'django_extensions',
+     'allauth.socialaccount.providers.google',
     'django.contrib.humanize',
     'crispy_forms',
 ]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -161,6 +175,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 
 
 # Internationalization
